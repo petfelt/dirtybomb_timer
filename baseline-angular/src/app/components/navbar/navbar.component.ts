f788697dcff560d839d522c4a266a1e04c486618
Peter Felton <peteraminfelton@gmail.com>
2018-02-02 16:57:48 -0800

    Updated to use correct bootstrap links, some cleaned up code.

--- baseline-angular/src/app/components/navbar/navbar.component.ts
+++ baseline-angular/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
-import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -9,24 +6,11 @@ import { FlashMessagesService } from 'angular2-flash-messages';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor(
-    private authService: AuthService,
-    private router:Router,
-    private flashMessage: FlashMessagesService,
-    ) { }
-
-  ngOnInit() {
+  selectedTab: string;
+  constructor() {
   }
 
-  onLogoutClick(){
-    this.authService.logout();
-    this.flashMessage.show('You are now logged out.', {
-      cssClass: 'alert-success',
-      timeout: 3000
-    });
-    this.router.navigate(['/login']);
-    return false;
+  ngOnInit() {
   }
 
 }
